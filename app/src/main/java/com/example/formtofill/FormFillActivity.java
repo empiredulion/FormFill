04a39a4af8f7ea90b3aab9b3bfb97ebc3a7e93d5
empiredulion <55353645+empiredulion@users.noreply.github.com>
2020-10-27 00:13:46 +0700

    Remove Unused Variables

--- app/src/main/java/com/example/formtofill/FormFillActivity.java
+++ app/src/main/java/com/example/formtofill/FormFillActivity.java
@@ -45,7 +45,7 @@ public class FormFillActivity extends AppCompatActivity {
 
         birthDay.setText(sdf.format(myCalendar.getTime()));
     }
-    
+
         @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
